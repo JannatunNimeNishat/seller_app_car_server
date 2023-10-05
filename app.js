72dcc7d4708329935aca6171be43220e1f4a6e06
JannatunNimeNishat <nishatnime100@gmail.com>
2023-10-05 11:43:55 +0600

    search and pagination api added

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const Cars = require('./assets/cars.json');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/search', (req, res) => {
+   
+    const searchValue = req.query.searchValue;
+   
+    if (searchValue) {
+        const regex = new RegExp(searchValue, 'i');
+        const searchedCars = Cars?.filter(car => regex.test(car?.model))
+        return res.send({ data: searchedCars })
+    }
+    return res.send({ data: Cars })
+   
+})
+
+app.get('/paginate', (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    // 
+    const startIndex = (page - 1) * limit;
+    const lastIndex = page * limit;
+
+    const result = {};
+
+    result.totalCars = Cars.length;
+
+    result.data = Cars.slice(startIndex, lastIndex);
+    res.send(result)
+
+})
+
+
+
+
+module.exports = app;
